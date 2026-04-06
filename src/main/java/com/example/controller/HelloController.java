b94caee13cfde18adfeb46f584c9721a4ce40fd9
lovwish125 <15546427+lovwish125@user.noreply.gitee.com>
2026-04-06 12:52:45 +0800

    go to auto build master

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello DevOps develop or master";
+        return "Hello DevOps develop or master2";
     }
 }
